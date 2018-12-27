7c6b85c807fd8143221b773b7d58356b257038aa
Kierán Meinhardt <kieran.meinhardt@gmail.com>
2018-12-27 15:02:04 +0100

    Rewrite

--- Makefile
+++
@@ -1,1 +0,0 @@
-.hstool/Makefile
--- news.hs
+++ news.hs
@@ -1,80 +1,108 @@
-{-# LANGUAGE OverloadedStrings #-}
-import Data.Monoid
-import Prelude hiding (elem)
-import Network.Wreq
-import Control.Lens
-import Text.Feed.Import
-import qualified Text.Atom.Feed as Atom
-import qualified Data.ByteString.Lazy.Char8 as LBS8
-import qualified Data.ByteString.Char8 as BS8
-import qualified Data.ByteString as BS
-import Text.RSS.Syntax
-import qualified Text.Feed.Types as Feed
-import qualified Data.BloomFilter as Bloom
-import qualified Data.BloomFilter.Easy as Bloom
-import qualified Data.BloomFilter.Hash as Bloom
-import Control.Concurrent
-import System.Environment
-import System.IO
-import Control.Exception
+{-# LANGUAGE DeriveGeneric, FlexibleContexts, LambdaCase, OverloadedStrings #-}
 
+import Control.Concurrent (forkIO, threadDelay)
+import Control.Concurrent.STM
+import Control.Monad (forM_, forever)
+import qualified Control.Monad.State as State
+import Control.Monad.Trans
+import Data.Aeson
+import Data.BloomFilter (Bloom)
+import qualified Data.BloomFilter as Bloom (fromList, insertList, notElem)
+import Data.BloomFilter.Hash (cheapHashes)
+import qualified Data.ByteString as BS (ByteString)
+import qualified Data.ByteString.Lazy.Char8 as LBS8 (readFile, unpack)
+import Data.Maybe (fromMaybe)
+import Data.Monoid ((<>))
+import qualified Data.Text as Text (pack)
+import Data.Text (Text)
+import qualified Data.Text.Encoding as Text (encodeUtf8)
+import GHC.Generics (Generic)
+import Lens.Micro ((&), (^.))
+import Network.IRC.Client
+import Network.IRC.Conduit
+import qualified Network.Wreq as Wreq (get, responseBody)
+import System.Environment (getArgs)
+import qualified Text.Atom.Feed as Atom
+import Text.Feed.Import (parseFeedString)
+import qualified Text.Feed.Types as Feed (Feed(..))
+import Text.RSS.Syntax (RSSItem(..), rssChannel, rssItems)
 
 data Item = Item
-    { ni_title :: BS.ByteString
-    , ni_link :: BS.ByteString
-    }
-  deriving Show
+  { ni_title :: Text
+  , ni_link :: Text
+  } deriving (Show)
 
+type Filter = Bloom BS.ByteString
 
-atomEntryToItems :: Atom.Entry -> [Item]
-atomEntryToItems entry =
-    map (Item title) links
+feedToItems :: Maybe Feed.Feed -> [Item]
+feedToItems =
+  \case
+    Just (Feed.RSSFeed rss) ->
+      concatMap rssItemToItems (rssItems (rssChannel rss))
+    Just (Feed.AtomFeed atom) ->
+      concatMap atomEntryToItems (Atom.feedEntries atom)
+    _ -> []
   where
-    title = BS8.pack $ Atom.txtToString (Atom.entryTitle entry)
-    links = map (BS8.pack . Atom.linkHref) (Atom.entryLinks entry)
+    atomEntryToItems entry = map (Item title) links
+      where
+        title = Text.pack $ Atom.txtToString (Atom.entryTitle entry)
+        links = map Atom.linkHref (Atom.entryLinks entry)
+    rssItemToItems item = map (Item title) links
+      where
+        title = fromMaybe "untitled" (rssItemTitle item)
+        links = maybe [] pure (rssItemLink item)
 
-rssItemToItems :: RSSItem -> [Item]
-rssItemToItems item =
-    map (Item title) links
-  where
-    title = maybe "untitled" BS8.pack (rssItemTitle item)
-    links = maybe [] ((:[]) . BS8.pack) (rssItemLink item)
+deduplicate :: TVar Filter -> [Item] -> STM [Item]
+deduplicate var items = do
+  bloom <- readTVar var
+  writeTVar var $ Bloom.insertList (map (Text.encodeUtf8 . ni_link) items) bloom
+  return $ filter (flip Bloom.notElem bloom . Text.encodeUtf8 . ni_link) items
 
+data Config = Config
+  { c_bots :: [Bot]
+  , c_channels :: [ChannelName Text]
+  } deriving (Generic)
+
+instance FromJSON Config where
+  parseJSON = genericParseJSON defaultOptions {fieldLabelModifier = drop 2}
+
+data Bot = Bot
+  { b_nick :: NickName Text
+  , b_feeds :: [String]
+  , b_delay :: Int
+  } deriving (Generic)
+
+instance FromJSON Bot where
+  parseJSON = genericParseJSON defaultOptions {fieldLabelModifier = drop 2}
 
 main :: IO ()
 main = do
-    hSetBuffering stdout LineBuffering
-    [url, delayString] <- getArgs
-    let delay = read delayString :: Int
-    let bloom0 = Bloom.fromList (Bloom.cheapHashes 17) (2^10 * 1000) [""]
-    rec url delay bloom0
-    return ()
+  let connectionC =
+        plainConnection "irc.r" 6667 & set username "news" & set flood 0
+  [configFile] <- getArgs
+  config <- decode <$> LBS8.readFile configFile
+  let bloom0 =
+        Bloom.fromList (cheapHashes 17) (2 ^ 10 * 1000) ["" :: BS.ByteString]
+  state <- atomically $ newTVar bloom0
+  forM_ (maybe [] c_bots config) $ \bot -> do
+    let instanceC =
+          defaultInstanceConfig (b_nick bot) &
+          set channels (maybe [] c_channels config)
+    ircState <- newIRCState connectionC instanceC state
+    _ <- forkIO $ runClientWith ircState
+    forkIO $
+      flip runIRCAction ircState $ do
+        st <- State.get
+        let cs = get channels instanceC
+        forever $
+          forM_ (b_feeds bot) $ \url -> do
+            r <- liftIO $ Wreq.get url
+            let f = parseFeedString $ LBS8.unpack $ r ^. Wreq.responseBody
+            items <- liftIO $ atomically $ deduplicate st $ feedToItems f
+            forM_ items $ \item -> forM_ cs $ \channel -> sendTo channel item
+            liftIO $ threadDelay (b_delay bot * 10 ^ 6)
+  forever $ threadDelay $ 10 ^ 6
   where
-    rec url delay bloom =
-      ( do
-        r <- get url
-        let f = parseFeedString $ LBS8.unpack $ r ^. responseBody
-            items =
-              case f of
-                Just (Feed.RSSFeed rss) ->
-                    concatMap rssItemToItems (rssItems (rssChannel rss))
-                Just (Feed.AtomFeed atom) ->
-                    concatMap atomEntryToItems (Atom.feedEntries atom)
-                _ -> []
-
-            bloom' = Bloom.insertList (map ni_link items) bloom
-            newLinkItems = filter (\item ->
-                    let link = ni_link item in
-                    link `Bloom.notElem` bloom
-                ) items
-        mapM_ BS8.putStrLn (map (\item ->
-                let title = ni_title item
-                    link = ni_link item
-                in title <> " " <> link
-            ) newLinkItems)
-        threadDelay (delay * 10^6)
-        rec url delay bloom'
-      )
-        `catch` \e -> hPutStrLn stderr ("Caught " ++ show (e :: SomeException)) >> rec url delay bloom
-
+    sendTo channel = send . Privmsg channel . Right . display
+    display (Item t l) = t <> " " <> l
 
