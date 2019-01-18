580d7f150c23afcc80566ea9f90e09ca75d1fd44
Kierán Meinhardt <kieran.meinhardt@gmail.com>
2019-01-18 11:54:13 +0100

    Shorten URLs if desired; optparse
    + Add option to shorten all feed item URLs using a specified shortening endpoint (fixes #1.)
    + Use optparse-applicative for CLI option parsing

--- src/Feed.hs
+++ src/Feed.hs
@@ -12,12 +12,12 @@ import qualified Data.ByteString as BS (ByteString)
 import qualified Data.BloomFilter as Bloom (insertList, notElem)
 import qualified Data.Text.Encoding as Text (encodeUtf8)
 
-data Item = Item
-  { ni_title :: Text
-  , ni_link :: Text
+data FeedItem = FeedItem
+  { fi_title :: Text
+  , fi_link :: Text
   } deriving (Show)
 
-feedToItems :: Maybe Feed.Feed -> [Item]
+feedToItems :: Maybe Feed.Feed -> [FeedItem]
 feedToItems =
   \case
     Just (Feed.RSSFeed rss) ->
@@ -26,18 +26,18 @@ feedToItems =
       concatMap atomEntryToItems (Atom.feedEntries atom)
     _ -> []
   where
-    atomEntryToItems entry = map (Item title) links
+    atomEntryToItems entry = map (FeedItem title) links
       where
         title = pack $ Atom.txtToString $ Atom.entryTitle entry
         links = map Atom.linkHref (Atom.entryLinks entry)
-    rssItemToItems item = map (Item title) links
+    rssItemToItems item = map (FeedItem title) links
       where
         title = fromMaybe "untitled" (rssItemTitle item)
         links = maybe [] pure (rssItemLink item)
 
-deduplicate :: TVar (Bloom BS.ByteString) -> [Item] -> STM [Item]
+deduplicate :: TVar (Bloom BS.ByteString) -> [FeedItem] -> STM [FeedItem]
 deduplicate var items = do
   bloom <- readTVar var
-  writeTVar var $ Bloom.insertList (map (Text.encodeUtf8 . ni_link) items) bloom
-  return $ filter (flip Bloom.notElem bloom . Text.encodeUtf8 . ni_link) items
+  writeTVar var $ Bloom.insertList (map (Text.encodeUtf8 . fi_link) items) bloom
+  return $ filter (flip Bloom.notElem bloom . Text.encodeUtf8 . fi_link) items
 
