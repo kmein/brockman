5c9d72e3883d3159d1f2defd577d63e1ecfde447
lassulus <lassulus@lassul.us>
2021-02-05 09:24:43 +0100

    use lrucaching for LRU

--- src/Brockman/Feed.hs
+++ src/Brockman/Feed.hs
@@ -7,7 +7,8 @@ module Brockman.Feed where
 import Control.Applicative (Alternative (..))
 import Data.Fixed
 import Data.List
-import qualified Data.Cache.LRU as LRU
+import qualified Data.LruCache as LRU
+import Data.LruCache.Internal (LruCache (lruCapacity))
 import Data.Hashable (hash)
 import Data.Maybe (fromMaybe, listToMaybe, mapMaybe)
 import Data.Text (Text, intercalate, lines, pack, strip, unwords)
@@ -24,7 +25,7 @@ import Text.RSS.Syntax (RSSItem (rssItemPubDate))
 import qualified Text.RSS.Syntax as RSS
 import qualified Text.RSS1.Syntax as RSS1
 
-type LRU = LRU.LRU Int ()
+type LRU = LRU.LruCache Int Bool
 
 data FeedItem = FeedItem
   { itemTitle :: Text,
@@ -81,19 +82,17 @@ deduplicate :: Maybe LRU -> [FeedItem] -> (LRU, [FeedItem])
 deduplicate maybeLRU items =
   case maybeLRU of
     Nothing ->
-      (freshLru, [])
+      freshLru
     Just lru ->
-      case (LRU.maxSize lru) of
-        Just size -> if size < genericLength items then
-            (freshLru, [])
-          else
-            foldl' step (lru, []) items
-        Nothing -> foldl' step (lru, []) items
+      if lruCapacity lru < genericLength items then
+        freshLru
+      else
+        insertItems lru items
   where
-    freshLru = LRU.fromList (Just $ genericLength items * 2) $ map (\i -> (key i, ())) items
+    freshLru = insertItems (LRU.empty (genericLength items * 2)) items
     key = hash . itemLink
+    insertItems lru items' = foldl' step (lru, []) items'
     step (lru, items') item =
-      let (newLru, maybeItem) = LRU.lookup (key item) lru
-      in case maybeItem of
-        Nothing -> (LRU.insert (key item) () newLru, item : items')
-        Just () -> (newLru, items')
+      case LRU.lookup (key item) lru of
+        Nothing -> (LRU.insert (key item) False lru, item : items')
+        Just (False, newLru) -> (newLru, items')
