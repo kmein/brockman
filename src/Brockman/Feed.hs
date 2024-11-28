fa06f9826c051972a54594d3d813f0638a50a612
Kierán Meinhardt <kmein@posteo.de>
2024-11-28 10:45:33 +0100

    Update src/Brockman/Feed.hs

--- src/Brockman/Feed.hs
+++ src/Brockman/Feed.hs
@@ -79,7 +79,7 @@ feedToItems = maybe [] (mapMaybe fromItem . feedItems)
             date = Atom.entryPublished entry
          in FeedItem title date <$> link
       Feed.RSSItem item ->
-        let title = fromMaybe (fromMaybe "untitled" (RSS.rssItemDescription item)) (RSS.rssItemTitle item)
+        let title = fromMaybe "untitled" (RSS.rssItemTitle item <|> RSS.rssItemDescription item)
             link = RSS.rssItemLink item
             date = RSS.rssItemPubDate item
          in FeedItem title date <$> link
