9fe93b07b306c747c3403b49597bf746db6fe400
Kierán Meinhardt <kmein@posteo.de>
2024-12-31 12:10:14 +0100

    move tests to flake check

--- flake.nix
+++ flake.nix
@@ -13,8 +13,8 @@
     supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
     forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
   in {
-    overlays.default = self: super: {
-      brockman = self.haskellPackages.callPackage ./default.nix {};
+    overlays.default = final: prev: {
+      brockman = prev.haskellPackages.callPackage ./default.nix {};
     };
 
     packages = forAllSystems (system: {
@@ -22,11 +22,15 @@
         inherit system;
         overlays = [ self.overlays.default ];
       }).brockman;
-      vm = nixos-generators.nixosGenerate {
-        pkgs = nixpkgs.legacyPackages.${system};
-        modules = [ self.nixosModules.default (import nix/vm.nix) ];
-        format = "vm-nogui";
-      };
+      # vm = nixos-generators.nixosGenerate {
+      #   pkgs = nixpkgs.legacyPackages.${system};
+      #   modules = [
+      #     { nixpkgs.overlays = [ self.overlays.default ]; }
+      #     self.nixosModules.default
+      #     (import nix/vm.nix)
+      #   ];
+      #   format = "vm-nogui";
+      # };
     });
 
     apps = forAllSystems (system: let
@@ -36,32 +40,35 @@
         type = "app";
         program = toString (pkgs.writeScript "generate" "${pkgs.cabal2nix}/bin/cabal2nix . > default.nix");
       };
-      fix-formatting = {
+      format = {
         type = "app";
         program = toString (pkgs.writeScript "fix-formatting" ''${pkgs.findutils}/bin/find src/ -type f -exec ${pkgs.ormolu}/bin/ormolu --mode inplace '{}' \;'');
       };
-      check-formatting = {
-        type = "app";
-        program = toString (pkgs.writeScript "check-formatting" ''${pkgs.findutils}/bin/find src/ -type f -exec ${pkgs.ormolu}/bin/ormolu --mode check '{}' \;'');
-      };
-      check-configs = {
-        type = "app";
-        program = toString (pkgs.writeScript "check-configs" ''
-          set -e
-          for config in ./config/*.json; do
-            echo === checking "$config"
-            ${self.defaultPackage.${system}}/bin/brockman --check "$config"
-          done
-        '');
-      };
     });
 
-    devShell = forAllSystems (system: let
+    checks = forAllSystems (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      formatting = pkgs.runCommand "formatting" {} ''
+        ${pkgs.findutils}/bin/find ${./src} -type f -exec ${pkgs.ormolu}/bin/ormolu --mode check '{}' \; > $out
+      '';
+      configs = pkgs.runCommand "configs" {} ''
+        set -e
+        for config in ${./config}/*.json; do
+          echo === checking "$config"
+          ${self.packages.${system}.default}/bin/brockman --check "$config"
+        done > $out
+      '';
+    });
+
+    devShells = forAllSystems (system: let
       pkgs = nixpkgs.legacyPackages.${system};
       package = pkgs.haskellPackages.callPackage ./default.nix {};
-    in package.env.overrideAttrs (old: old // {
-      buildInputs = [ pkgs.cabal-install ];
-    }));
+    in {
+      default = package.env.overrideAttrs (old: old // {
+        buildInputs = [ pkgs.cabal-install ];
+      });
+    });
 
     nixosModules.default = { ... }: {
       imports = [nix/module.nix];
